dde134934f13cba2606a50fe70f729f86bcfbcd0
Maxime <maxime.b2494@gmail.com>
2025-02-06 22:24:20 +0100

    1.6 : Les fonctions / en cours

---
+++ Chapitre 1/1.6 Les fonctions/script.js
@@ -0,0 +1,18 @@
+// 📌 Exercices pratiques :
+// Exercice 1 : Fonction de bienvenue
+// Crée une fonction bienvenueUtilisateur qui prend un prénom en paramètre et affiche "Bienvenue, [prénom] !".
+
+
+// Exercice 2 : Fonction avec retour de valeur
+// Crée une fonction carre qui prend un nombre en paramètre et retourne son carré.
+// Affiche le résultat dans la console.
+
+
+// Exercice 3 : Fonction fléchée
+// Écris une fonction fléchée soustraction qui prend deux nombres et retourne leur différence.
+// Teste-la avec quelques valeurs.
+
+
+// Exercice 4 : Callback et fonction anonyme
+// Utilise setTimeout() pour afficher "Message affiché après 3 secondes".
+// Utilise une fonction anonyme dans setTimeout().
